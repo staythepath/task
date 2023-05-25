99dd283370af10a9557f1d3e5439d15f778c47c0
staythepath <landonmitchell@gmail.com>
2023-05-25 02:06:09 -0500

    significant

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ function Home({ todos, setTodos }) {
       <div style={{ position: "relative", textAlign: "center" }}>
         <h4 style={{ textAlign: "center" }}>
           Here is the home page. And a bunch of text about something Idk what
-          I'll put here
+          I'll put here. I just know that all of this is significant to me.
         </h4>
       </div>
     </div>
