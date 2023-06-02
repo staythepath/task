a034244f7378425516c81331979f6038f4c64c7f
staythepath <landonmitchell@gmail.com>
2023-06-02 09:38:37 -0500

    added registration page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-
 function Home({ todos, setTodos }) {
   return (
     <div className="ToDoList">
       <h1>Home</h1>
       <div style={{ position: "relative", textAlign: "center" }}>
         <h4 style={{ textAlign: "center" }}>
-          I'll put something better here later, but right now I'm just letting you know, you have to log in to use any of this.
+          I'll put something better here later, but right now I'm just letting
+          you know, you have to log in to use any of this.
         </h4>
       </div>
     </div>
